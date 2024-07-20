c2502e5d099eed149c630b979349b35390f22f42
Freja Roberts <ten3roberts@gmail.com>
2024-07-20 15:31:04 +0200

    fix: implement std::error::Error for error types

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,27 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
-    IOError(std::io::Error),
+    #[error("IO Error")]
+    IOError(#[from] std::io::Error),
+    #[error("Parsing failed")]
     ParseError(ParseError, usize),
+    #[error("Found extra content before the root node")]
     ContentOutsideRoot,
+    #[error("No such tag {1:?} inside {0:?}")]
     TagNotFound(String, String),
+    #[error("No such attribute {1:?} inside {0:?}")]
     AttributeNotFound(String, String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ParseError {
+    #[error("Missing closing tag for {0:?}")]
     MissingClosingTag(String),
+    #[error("Missing closing delimiter")]
     MissingClosingDelimiter,
+    #[error("Missing attribute value for {0:?}")]
     MissingAttributeValue(String),
+    #[error("Missing quotes for {0:?}")]
     MissingQuotes(String),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Error::IOError(e)
-    }
 }
